c1e3fb73bc2c0432b6c87a39f8f4a8f272a271b5
tschlag <travis.schlageter@gmail.com>
2023-03-20 21:12:00 -0600

    Updated file structure

--- lambda_terraform/main.tf
+++ lambda_terraform/main.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch" {
+resource "aws_lambda_permission" "allow_invoke" {
   statement_id  = "AllowLambdaExecution"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.public_lambda.function_name
@@ -24,10 +24,8 @@ resource "aws_lambda_permission" "allow_cloudwatch" {
 }
 
 resource "aws_lambda_function" "public_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "DivvyCloud-Org-CloudTrail-Lambda.zip"
-  function_name = "public_lambda_test"
+  filename      = "Lambda-Code.zip"
+  function_name = "eks_lambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.lambda_handler"
 
