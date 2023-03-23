c36e1a24c0dc9ae1c52f67d129299ddcc85b131e
tschlag <travis.schlageter@gmail.com>
2023-03-23 08:38:00 -0600

    Updated Lambda TF

--- lambda_terraform/main.tf
+++ lambda_terraform/main.tf
@@ -47,7 +47,8 @@ resource "aws_lambda_function" "public_lambda" {
     variables = {
       env = "dev",
       api_key = "super_secret",
-      test = "key"
+      test = "key",
+      owner = "Travis"
     }
   }
 }
